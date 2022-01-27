22dd212bc556190cd86747b3092c7b9e64f542ba
Ashton Gasser <ashtongasser@gmail.com>
2022-01-27 16:35:31 -0600

    refactored some code

--- dice.py
+++ dice.py
@@ -76,15 +76,35 @@ def roll_dice(num_dice):
 
 def generate_dice_faces_diagram(dice_values):
     """Return an ASCII diagram of dice faces from `dice_values`.
-     The string returned contains an ASCII representation of each die.
+    The string returned contains an ASCII representation of each die.
+    For example, if `dice_values = [4, 1, 3, 2]` then the string
+    returned looks like this:
+    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
+    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
+    │  ●   ●  │ │         │ │  ●      │ │  ●      │
+    │         │ │    ●    │ │    ●    │ │         │
+    │  ●   ●  │ │         │ │      ●  │ │      ●  │
+    └─────────┘ └─────────┘ └─────────┘ └─────────┘
     """
-  # Generate a list of dice faces from DICE_ART
+    dice_faces = _get_dice_faces(dice_values)
+    dice_faces_rows = _generate_dice_faces_rows(dice_faces)
 
+    # Generate header with the word "RESULTS" centered
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
+
+def _get_dice_faces(dice_values):
     dice_faces = []
     for value in dice_values:
         dice_faces.append(DICE_ART[value])
+    return dice_faces
 
-        # Generate a list containing the dice faces rows
+
+def _generate_dice_faces_rows(dice_faces):
     dice_faces_rows = []
     for row_idx in range(DIE_HEIGHT):
         row_components = []
@@ -92,13 +112,7 @@ def generate_dice_faces_diagram(dice_values):
             row_components.append(die[row_idx])
         row_string = DIE_FACE_SEPARATOR.join(row_components)
         dice_faces_rows.append(row_string)
-
-        # Generate header with the word "RESULTS" centered
-    width = len(dice_faces_rows[0])
-    diagram_header = " RESULTS ".center(width, "~")
-
-    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
-    return dice_faces_diagram
+    return dice_faces_rows
 
 
 # ~~~ App's main code block ~~~
@@ -107,8 +121,7 @@ num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
 # 2. Roll the dice
 roll_results = roll_dice(num_dice)
-# 3. Generate the ASC11 diagram faces
+# 3. Generate the ASCII diagram of dice faces
 dice_face_diagram = generate_dice_faces_diagram(roll_results)
 # 4. Display the diagram
 print(f"\n{dice_face_diagram}")
-# print(roll_results)  # Remove this line after testing the app
